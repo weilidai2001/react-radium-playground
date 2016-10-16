87af9761b15fa75ff061ccf22b707f270f446521
weilidai <weilidai2001@hotmail.com>
2016-10-16 19:33:01 +0100

    Refactoring

--- src/actions/property-actions.js
+++ src/actions/property-actions.js
@@ -2,15 +2,13 @@ import * as types from './action-types';
 import propertyApi from '../api/property-api';
 
 export function loadPropertiesSuccess(properties) {
-  return { type: types.LOAD_PROPERTIES_SUCCESS, properties};
+  return {type: types.LOAD_PROPERTIES_SUCCESS, properties};
 }
 
 export function loadProperties() {
-  return function(dispatch) {
-    return propertyApi.getAllProperties().then(properties => {
-      dispatch(loadPropertiesSuccess(properties));
-    }).catch(error => {
-      throw(error);
-    });
-  };
+  return dispatch =>
+    propertyApi
+      .getAllProperties()
+      .then(properties => dispatch(loadPropertiesSuccess(properties)))
+      .catch(error => {throw(error)});
 }
