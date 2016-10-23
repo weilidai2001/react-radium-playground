d05a4ecb6874cc4454d945f6477d4444644b0ce5
Weili Dai <Weili.Dai@sky.uk>
2016-10-23 18:57:55 +0100

    Store on server-side

--- server/routes/liuinternational.js
+++ server/routes/liuinternational.js
@@ -1,5 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+
 const router = express.Router();
+
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.use(bodyParser.json());
+router.use(compression());
 
 const PropertiesDao = require('../properties-dao');
 const propertiesDao = new PropertiesDao();
@@ -7,6 +16,18 @@ const propertiesDao = new PropertiesDao();
 module.exports = function() {
   router.get('/properties', (req, res) => {
     res.send(propertiesDao.getState());
+  });
+
+  router.put('/properties/:id', (req, res) => {
+    propertiesDao.updatePropertyById(req.params.id, req.body);
+    res.send({status: 'OK'});
+  });
+
+  router.post('/properties', (req, res) => {
+    res.send({
+      status: 'OK',
+      id: propertiesDao.insertProperty(req.body)
+    });
   });
 
   return router;
--- src/reducers/property-reducer.js
+++ src/reducers/property-reducer.js
@@ -1,22 +1,24 @@
 import * as types from '../actions/action-types';
 import initialState from './initial-state';
 
+const sortRule = (a, b)=> a.order - b.order || b.id - a.id;
+
 export default function propertyReducer(state = initialState.properties, action) {
   switch (action.type) {
     case types.LOAD_PROPERTIES_SUCCESS:
-      return action.properties;
+      return action.properties.sort(sortRule);
 
     case types.CREATE_PROPERTY_SUCCESS:
       return [
         ...state,
         Object.assign({}, action.property)
-      ];
+      ].sort(sortRule);
 
     case types.UPDATE_PROPERTY_SUCCESS:
       return [
         ...state.filter(property => property.id !== action.property.id),
         Object.assign({}, action.property)
-      ];
+      ].sort(sortRule);
 
     default:
       return state;
