75cf63e086c1b5a035e31d78fea418ba00cfdb09
weilidai <weilidai2001@hotmail.com>
2016-10-23 11:40:29 +0100

    Added Bootstrap and installed dependent npm

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {Router, browserHistory} from 'react-router';
 
 import {loadProperties} from './actions/property-actions';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
