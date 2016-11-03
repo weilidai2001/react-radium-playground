56caf270e57a4ccfb862d7dfd3ea782eafd6fce4
Weili Dai <Weili.Dai@sky.uk>
2016-11-03 10:09:18 +0000

    Styling

--- src/components/common/image-upload.js
+++ src/components/common/image-upload.js
@@ -3,9 +3,8 @@ import React, {PropTypes} from 'react';
 const ImageUpload = ({onUploadFileChanged, onSubmitClicked}) => {
   return (
     <div className="image-editor-upload">
-      <input className="image-editor-upload__filename" type="file" onChange={onUploadFileChanged} name="image-upload"/>
-      <input className="image-editor-upload__submit" type="submit" onClick={onSubmitClicked} value="Upload Image"
-             name="submit"/>
+      <input className="image-editor-upload__filename btn" type="file" onChange={onUploadFileChanged} name="image-upload"/>
+      <input className="image-editor-upload__submit btn btn-primary" type="submit" onClick={onSubmitClicked} value="Upload Image" name="submit"/>
     </div>
   );
 };
