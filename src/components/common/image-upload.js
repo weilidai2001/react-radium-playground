f95ed0cb2e7bd8977acecb36ed97a62a546076d1
weilidai <weilidai2001@hotmail.com>
2016-11-05 14:48:44 +0000

    Thumbnail upload

--- src/components/common/image-upload.js
+++ src/components/common/image-upload.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 
 const ImageUpload = ({onUploadFileChanged, onSubmitClicked}) => {
   return (
-    <div className="image-editor-upload">
+    <div className="image-editor-upload btn-toolbar panel panel-default" style={{padding: "5px"}}>
       <input className="image-editor-upload__filename btn" type="file" onChange={onUploadFileChanged} name="image-upload"/>
       <input className="image-editor-upload__submit btn btn-primary" type="submit" onClick={onSubmitClicked} value="Upload Image" name="submit"/>
     </div>
